9f6934112cb7c5db9f8a6610969b6bee4f757e6c
shristi482004 <shristish484@gmail.com>
2026-01-06 23:27:29 +0530

    deployed using firebase

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,5 +1,5 @@
 import { Navigate } from "react-router-dom";
-import { useAuth } from "/Users/shristishristi/Desktop/civix/Civix/src/context/Authcontext.jsx";
+import { useAuth } from "../context/Authcontext";
 
 const ProtectedRoute = ({ children }) => {
   const { user } = useAuth();
